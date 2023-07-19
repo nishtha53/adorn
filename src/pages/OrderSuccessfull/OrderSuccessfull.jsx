3c7547e4a672087f4fb96e6330fe0ad98bde6f00
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2023-07-19 22:41:20 +0530

    fix: text of order successfull page

--- src/pages/OrderSuccessfull/OrderSuccessfull.jsx
+++ src/pages/OrderSuccessfull/OrderSuccessfull.jsx
@@ -4,7 +4,7 @@ function OrderSuccessfull() {
   return (
     <div style={{textAlign:"center"}}>
     <img src="https://www.bing.com/th/id/OGC.57b54818e2011d482548fb54201ce6c1?pid=1.7&rurl=https%3a%2f%2fi.stack.imgur.com%2fGQFRr.gif&ehk=mZYWG5s%2fS%2bl0gyl97KxH3Uqs%2fVb78WS%2fUQlTYQS388U%3d" alt="Order Success GIF" height={500} width={500}/>
-    <h1 style={{color:"#832729"}}>Order Placed Successfully!</h1>
+    <h1 style={{color:"#832729"}}>Congratulations, your order is placed successfully! 🎉</h1>
     <p style={{margin:"5px"}}>Thank you for your purchase. Your order has been placed successfully.</p>
   </div>
 
